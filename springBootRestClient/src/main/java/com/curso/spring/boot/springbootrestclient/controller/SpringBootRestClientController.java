c22d90d341dcc909bce9a8ee28414b3c0461d2c0
joseramonperez <39797585+joseramonperez@users.noreply.github.com>
2019-04-09 09:40:49 +0200

    Actualizando servicio rest

---
+++ springBootRestClient/src/main/java/com/curso/spring/boot/springbootrestclient/controller/SpringBootRestClientController.java
@@ -0,0 +1,12 @@
+package com.curso.spring.boot.springbootrestclient.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SpringBootRestClientController {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
